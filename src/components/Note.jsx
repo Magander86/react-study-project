5dd6d8dcc24e9f0b6f11229b9211c27dd470ff15
Magander86 <anderson.magal.30@gmail.com>
2022-01-16 11:04:28 -0300

    Criar e estilizar site inicial

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>Título da anotação</h1>
+            <p>Conteúdo da anotação</p>
+        </div>        
+    );
+}
+
+export default Note;
